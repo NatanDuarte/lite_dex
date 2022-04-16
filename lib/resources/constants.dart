825f56f1c5c8fc80ecf59adbc4275f53db69148c
NatanDuarte <natanprocopio@gmail.com>
2022-04-16 14:19:36 -0300

    constants class created for better organization

---
+++ lib/resources/constants.dart
@@ -0,0 +1,6 @@
+class Constants {
+  static const String appName = 'LiteDex';
+  static const String findPokemonFormHintText = 'Type a pokemon name or number';
+  static const String submitButtonText = 'Find!';
+  static const String pokemonNotFound = 'Pokemon not found 😞';
+}

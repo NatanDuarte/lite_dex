f2c4e1ef2093eb7e7cdf507d9dcc2b053b80a0f9
NatanDuarte <natanprocopio@gmail.com>
2022-04-17 18:44:09 -0300

    app main class extracted to file

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:lite_dex/http/webclient.dart';
-import 'package:lite_dex/resources/constants.dart';
-import 'package:lite_dex/screens/find_pokemon.dart';
-import 'package:lite_dex/screens/pokemon_info.dart';
+import 'package:lite_dex/screens/lite_dex.dart';
 
 void main() {
   runApp(const LiteDex());
-}
-
-class LiteDex extends StatelessWidget {
-  const LiteDex({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-
-    return MaterialApp(
-      title: Constants.appName,
-      theme: ThemeData(
-        primarySwatch: Colors.red,
-      ),
-      home: Scaffold(
-        key: _formKey,
-        appBar: AppBar(title: const Text(Constants.appName)),
-        body: FindPokemonByName(key: key),
-      ),
-    );
-  }
 }
